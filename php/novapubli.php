aae556392c96fd9cdcc2852dc2bd0c5f8c2b207e
Josue-RR <152731891+Josue-RR@users.noreply.github.com>
2023-12-20 20:50:24 -0300

    Add files via upload

---
+++ php/novapubli.php
@@ -0,0 +1,47 @@
+<?php
+
+include_once("conexao.php");
+
+$titulo = $_POST['titulo'];
+$descricao = $_POST['descricao'];
+$esporte = $_POST['esporte'];
+
+
+if (isset($_FILES['arquivo'])) {
+    $arquivo = $_FILES['arquivo'];
+
+    if ($arquivo['error'])
+        die("Falha ao enviar");
+
+    $pasta = "anexos/";
+    $nomeArquivo = $arquivo['name'];
+    $novoNomeArquivo = uniqid();
+    $extensao = strtolower(pathinfo($nomeArquivo, PATHINFO_EXTENSION));
+
+    if ($extensao != "jpg" && $extensao != "png")
+        die("Tipo de arquivo não aceito");
+
+    $url = $pasta . $novoNomeArquivo . "." . $extensao;
+    $deu_certo = move_uploaded_file($arquivo["tmp_name"], $url);
+    if ($deu_certo) {
+        $sql_post = "INSERT INTO post (titulo_post, descricao_post, url_imagem) VALUES ('$titulo', '$descricao', '$url')";
+        $conexao->query($sql_post);
+        echo " Arquivo enviado com sucesso";
+    } else {
+        echo " Falha ao enviar";
+    }
+}
+
+$sql_esporte = "INSERT INTO esp (nome_esp) VALUES ('$esporte')";
+$conexao->query($sql_esporte);
+
+
+
+
+header('Location: ../index.php');
+    exit();
+
+
+
+
+?>
